bbf6d79800fae6679fec3d843e50470558efbf3b
Joao Pedro <joaopioonze@gmail.com>
2026-06-11 17:13:52 -0300

    feat: configura o banco de dados via migration alembic

---
+++ backend/src/auth/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, DateTime, ForeignKey, Numeric
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+from decimal import Decimal
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "tb_users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class Transaction(Base):
+    __tablename__ = 'tb_transactions'
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    value: Mapped[Decimal] = mapped_column(Numeric(10,2), nullable=False)
+    type: Mapped[str] = mapped_column(String, nullable=False)
+    tag: Mapped[str] = mapped_column(String, nullable=False)
+    date: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False
+    )
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("tb_users.id", ondelete="CASCADE"), nullable=False
+    )
